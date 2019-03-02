2aeb8647230800d564338120e0ef7e2c9cf643d0
Annesha17 <annesha.k17@gmail.com>
2019-03-02 23:31:12 +0530

    added error handler

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-//const errorHandler = require("./handlers/error");
+const errorHandler = require("./handlers/error");
 const PORT = 3000;
 
 app.use(cors());
@@ -13,6 +13,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, function() {
   console.log(`Server is starting on port ${PORT}`);
